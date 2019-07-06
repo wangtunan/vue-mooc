ea6e3b30eab057d7b6d80b230f1fc3153ce6ce33
wangtunan <why583440138@gmail.com>
2019-07-06 13:09:29 +0800

    添加gh-pages分支

---
+++ depoly.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'depoly脚本更新'
+git remote add origin https://github.com/wangtunan/vue-mooc.git
+git config user.name 'wangtunan'
+git config user.email 'why583440138@gmail.com'
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:wangtunan/vue-mooc.git:gh-pages
