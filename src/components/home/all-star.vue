9ef636c7c5a93417d02e7214b3d0c80436f71ad9
wangtunan <why583440138@gmail.com>
2019-07-09 22:46:12 +0800

    全明星模块完善

--- src/components/home/all-star.vue
+++ src/components/home/all-star.vue
@@ -18,8 +18,15 @@
         </li>
       </ul>
       <ul class="all-star-list star">
-        <li class="all-item star-item" v-for="(item,index) in allstar.data" :key="index">
+        <li class="all-item star-item" v-for="(item,index) in allstar.data" :key="index" :class="getItemclass(item.type.code)">
           <img :src="item.avatar" width="48" height="48" alt="">
+          <div class="star-info">
+            <p class="star-type">="{{item.type.text}}"=</p>
+            <p class="star-name ellipsis">{{item.name}}</p>
+            <p class="star-number" v-if="item.type.code==1">一周获得{{item.number}}积分</p>
+            <p class="star-number" v-if="item.type.code==2">一周发布{{item.number}}篇手记</p>
+            <p class="star-number" v-if="item.type.code==3">一周解题{{item.number}}个</p>
+          </div>
         </li>
       </ul>
     </div>
@@ -34,10 +41,20 @@ export default {
     }
   },
   methods: {
+    // 获取背景图片的位置
     getPositionStyle(index) {
       return {
         'background-position': `center -${index * BASE_WIDTH}px`
       }
+    },
+    // 获取样式
+    getItemclass(code) {
+      const classMap = {
+        "1": "purple",
+        "2": "blue",
+        "3": "green"
+      }
+      return classMap[code]
     }
   }
 }
@@ -50,43 +67,102 @@ export default {
       background-position: center -504px;
     .right-icon
       background-position: center -540px;
-  .star
-    display: flex;
-    align-items: center;
-    .star-item
-      position: relative;
-      text-align: center;
-      cursor: pointer;
-      & > img
-        border-radius: 50%;
-    &.best-star
-      margin-bottom: 48px;
-      justify-content: center;
+  .all-star
+    padding-bottom: 36px;
+    .star
+      display: flex;
+      align-items: center;
       .star-item
-        width: 136px;
-        .star-name
-          margin-top: 8px;
-          font-size: 16px;
-          line-height: 16px;
-          color: #07111B;
-        .star-type
-          line-height: 16px;
-          font-size: 12px;
-          color: #93999F;
-        .star-crown
-          position: absolute;
-          left: 50%;
-          top: -15px;
-          width: 26px;
-          height: 18px;
-          margin-left: -13px;
-          background: url('https://www.imooc.com/static/img/index/champion.png') no-repeat
-          background-size: cover;
-    &.all-star-list
-      margin: 0 auto;
-      width: 880px;
-      flex-wrap: wrap;
-      justify-content: center;
-      .all-item
-        margin: 0 28px 24px 0;
+        position: relative;
+        text-align: center;
+        cursor: pointer;
+        & > img
+          padding: 3px;
+          border-radius: 50%;
+          transition: all 0.2s;
+      &.best-star
+        margin-bottom: 48px;
+        justify-content: center;
+        .star-item
+          width: 136px;
+          .star-name
+            margin-top: 8px;
+            font-size: 16px;
+            line-height: 16px;
+            color: #07111B;
+          .star-type
+            line-height: 16px;
+            font-size: 12px;
+            color: #93999F;
+          .star-crown
+            position: absolute;
+            left: 50%;
+            top: -12px;
+            width: 26px;
+            height: 18px;
+            margin-left: -13px;
+            background: url('https://www.imooc.com/static/img/index/champion.png') no-repeat
+            background-size: cover;
+      &.all-star-list
+        margin: 0 auto;
+        width: 880px;
+        flex-wrap: wrap;
+        justify-content: center;
+        .all-item
+          position: relative;
+          margin: 0 28px 24px 0;
+          .star-info
+            display: none;
+            position: absolute;
+            left: -82px;
+            top: -110px;
+            padding: 13px 24px 0;
+            width: 202px;
+            height: 90px;
+            border-radius: 8px;
+            box-sizing: border-box;
+            color: #fff;
+            &::after
+              content: '';
+              position: absolute;
+              left: 50%;
+              bottom: -16px;
+              width: 0;
+              height: 0;
+            .star-type
+              font-size: 12px;
+              line-height: 16px;
+            .star-name
+              font-size: 14px;
+              line-height: 24px;
+            .star-number
+              margin-top: 8px;
+              font-size: 12px;
+          &.purple:hover
+            img
+              box-shadow: 0 0 0 2px #663ed8;
+            .star-info
+              display: block;
+              background: #663ed8;
+              &::after
+                border: 8px solid transparent
+                border-color: #663ed8 transparent transparent transparent;
+          &.blue:hover
+            img
+              box-shadow: 0 0 0 2px #068CC6;
+            .star-info
+              display: block;
+              background: #068CC6;
+              &::after
+                border: 8px solid transparent
+                border-color: #068CC6 transparent transparent transparent;
+          &.green:hover
+            img
+              box-shadow: 0 0 0 2px #06b571;
+            .star-info
+              display: block;
+              background: #06b571;
+              &::after
+                border: 8px solid transparent
+                border-color: #06b571 transparent transparent transparent;
 </style>
