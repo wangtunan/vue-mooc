b4dde778a21bd7c044b8323d0de2d9aaecad7372
wangtunan <why583440138@gmail.com>
2019-08-07 23:51:45 +0800

    课程详情页面开发

--- src/components/course-detail/course-detail-header.vue
+++ src/components/course-detail/course-detail-header.vue
@@ -1,31 +1,26 @@
 <template>
   <div class="course-detail-header">
-    <div class="bg">
-      <img src="https://img4.mukewang.com/5ad5cc4a0001950c06000338.jpg" alt="">
+    <div class="header">
+      <div class="header-content m-center">
+        <p>
+          <span>课程 \ 前端开发 \ Vue.js \ vue2.5入门</span>
+          <span>
+            <i class="iconfont">&#xe610;</i>
+            <i class="iconfont"></i>
+            <i class="iconfont"></i>
+            <i class="iconfont"></i>
+          </span>
+        </p>
+      </div>
     </div>
-    <div class="detail-content m-center"></div>
   </div>
 </template>
 <style lang="stylus" scoped>
   .course-detail-header
-    .bg
-      position: relative;
-      width: 100%;
-      height: 200px;
-      text-align: center;
-      overflow: hidden;
-      background-image: linear-gradient(90deg, #19172D, #645DB7);
-      &::after
-        content: '';
-        position: absolute;
-        left: 0;
-        top: 0;
-        bottom: 0;
-        right: 0;
-        background-color: rgba(0,0,0,0.2);
-      & > img
-        display: inline-block;
+    height: 200px;
+    background-image: linear-gradient(90deg, #19172D, #645DB7);
+    .header
+      .header-content
         height: 100%;
-        filter: blur(20px);
-        transform: scale(3, 2)
+        color: #fff;
 </style>
