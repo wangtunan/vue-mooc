9ae801032db037fd43cdb2df12940a249a243414
wangtunan <why583440138@gmail.com>
2019-08-24 23:28:55 +0800

    专栏列表页面开发

--- src/base/header/nav.vue
+++ src/base/header/nav.vue
@@ -20,7 +20,7 @@ export default {
     this.navList = [
       { title: '课程', path: '/course' },
       { title: '实战课程', path: '/lesson' },
-      { title: '专栏', icon: 'https://www.imooc.com/static/img/common/new.png', path: '/' },
+      { title: '专栏', icon: 'https://www.imooc.com/static/img/common/new.png', path: '/read' },
       { title: '猿问', path: '/' },
       { title: '手记', path: '/' }
     ]
