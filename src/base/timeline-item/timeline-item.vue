fea5132dc11eaa062a906bec41bea5ec38f6761f
wangtunan <why583440138@gmail.com>
2019-09-10 23:09:37 +0800

    timeline组件完善

--- src/base/timeline-item/timeline-item.vue
+++ src/base/timeline-item/timeline-item.vue
@@ -1,17 +1,33 @@
 <template>
   <li class="timeline-item">
-    <div class="timeline-item-dot"></div>
+    <div class="timeline-item-line"></div>
+
+    <div
+      class="timeline-item-dot"
+      v-if="!$slots.dot"
+      :style="{
+        'background-color': color
+      }"
+      :class="[
+        `timeline-item-dot-${size || ''}`,
+        `timeline-item-dot-${type || ''}`
+      ]">
+    </div>
+
+    <div class="timeline-item-dot-customer" v-if="$slots.dot">
+      <slot name="dot"></slot>
+    </div> 
+
     <div class="timeline-item-wrapper">
+      <div v-if="!hideTimestamp && placement==='top'" class="timeline-item-timestamp top">
+        {{ timestamp }}
+      </div>
+      
       <div class="timeline-item-content">
         <slot></slot>
       </div>
-      <div
-        class="timeline-item-timestamp"
-        :class="{
-          'top': placement === 'top',
-          'bottom': placement === 'bottom'
-        }"
-        >
+
+      <div v-if="!hideTimestamp && placement==='bottom'" class="timeline-item-timestamp bottom">
         {{ timestamp }}
       </div>
     </div>
@@ -26,19 +42,32 @@ export default {
       type: String,
       default: ''
     },
+    hideTimestamp: {
+      type: Boolean,
+      default: false
+    },
     placement: {
       type: String,
-      default: 'top',
+      default: 'bottom',
       validator (val) {
         return ['top', 'bottom'].includes(val)
       }
     },
     size: {
       type: String,
-      default: 'normal',
+      default: 'small',
       validator (val) {
         return ['small', 'normal', 'medium', 'large'].includes(val)
       }
+    },
+    type: {
+      type: String,
+      validator (val) {
+        return ['primary', 'success', 'warning', 'danger', 'info'].includes(val)
+      } 
+    },
+    color: {
+      type: String
     }
   }
 }
@@ -49,36 +78,66 @@ export default {
   
   .timeline-item
     position: relative;
-    padding-bottom: $timeline-item-bottom;
+    padding-bottom: $timeline-item-padding-bottom;
+    &:last-child
+      .timeline-item-line
+        display: none;
+    &-line
+      position: absolute;
+      left: 4px;
+      top: 0;
+      height: 100%;
+      border-left: 2px solid $timeline-item-dot-color; 
     &-dot
       position: absolute;
       top: 0;
       border-radius: 50%;
-      background-color: $base-info;
+      background-color: $timeline-item-dot-color;
       display: flex;
       justify-content: center;
       align-items: center;
-      &.small
-        left: 0;
+      &-small
+        left: -1px;
         width: $timeline-item-dot-small-size;
         height: $timeline-item-dot-small-size;
-      &.normal
-        left: 0
+      &-normal
+        left: -2px;
         width: $timeline-item-dot-normal-size;
         height: $timeline-item-dot-normal-size;
-      &.medium
-        left: 0
+      &-medium
+        left: -3px
         width: $timeline-item-dot-medium-size;
         height: $timeline-item-dot-medium-size;
-      &.large
-        left: 0
+      &-large
+        left: -4px
         width: $timeline-item-dot-large-size;
         height: $timeline-item-dot-large-size;
+      &-primary
+        background-color: $base-primary;
+      &-success
+        background-color: $base-success;
+      &-warning
+        background-color: $base-warning;
+      &-danger
+        background-color: $base-danger;
+      &-info
+        background-color: $base-info;
+    &-dot-customer
+      position: absolute;
+      display: flex;
+      justify-content: center;
+      align-items: center;
     &-wrapper
       position: relative;
-      top: -3px;
+      top: 0;
       padding-left: $timeline-item-wrapper-padding-left;
-      &.timeline-item-timestamp
+      .timeline-item-content
         color: $timeline-item-title-color;
+      .timeline-item-timestamp
+        color: $base-info;
         line-height: 1;
+        &.top
+          margin-bottom: 8px;
+        &.bottom
+          margin-top: 8px;
 </style>
--- src/pages/test/test.vue
+++ src/pages/test/test.vue
@@ -1,13 +1,30 @@
 <template>
   <div class="test">
-    <!-- <timeline>
-      <timeline-item></timeline-item>
-    </timeline> -->
-    <el-timeline>
-      <el-timeline-item type="primary" timestamp="2016-09-10">创业</el-timeline-item>
-      <el-timeline-item timestamp="2017-09-11">快速发展</el-timeline-item>
-      <el-timeline-item timestamp="2018-09-10">上市</el-timeline-item>
-      <el-timeline-item timestamp="2019-09-10">至今</el-timeline-item>
+    <timeline :reverse="true">
+      <timeline-item size="small" placement="top" color="#58a" timestamp="2016-09-10">
+        <div class="content">创业</div>
+        <div class="dot" slot="dot">1</div>
+      </timeline-item>
+      <timeline-item size="small" placement="top" type="success" timestamp="2017-09-11">
+        <div class="content">快速发展</div>
+        <div class="dot" slot="dot">2</div>
+      </timeline-item>
+      <timeline-item size="small" placement="top" type="warning" timestamp="2018-09-10">上市</timeline-item>
+      <timeline-item size="small" placement="top" type="danger" timestamp="2019-09-10">至今</timeline-item>
+    </timeline>
+
+    
+    <el-timeline :reverse="true">
+      <el-timeline-item size="normal" placement="top" color="#58a" timestamp="2016-09-10">
+        <div class="content">创业</div>
+        <div class="dot" slot="dot">1</div>
+      </el-timeline-item>
+      <el-timeline-item size="normal" placement="top" type="success" timestamp="2017-09-11">
+        <div class="content">快速发展</div>
+        <div class="dot" slot="dot">2</div>
+      </el-timeline-item>
+      <el-timeline-item size="normal" placement="top" type="warning" timestamp="2018-09-10">上市</el-timeline-item>
+      <el-timeline-item size="normal" placement="top" type="danger" timestamp="2019-09-10">至今</el-timeline-item>
     </el-timeline>
   </div>
 </template>
@@ -28,6 +45,22 @@ export default {
     margin-left: 50px;
     margin-top: 50px;
     min-height: 800px;
-    & > div
+    & > ul
       margin: 20px 0;
+    .content
+      width: 400px;
+      height: 100px;
+      box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
+      padding: 20px;
+      box-sizing: border-box;
+    .dot
+      position: relative;
+      left: -6px;
+      width: 24px;
+      height: 24px;
+      background-color: #f60;
+      border-radius: 50%;
+      text-align: center;
+      line-height: 24px;
+      color: #fff;
 </style>
