14ade6ccf655e13aabb886cb99725a29a0393960
wangtunan <why583440138@gmail.com>
2019-09-15 22:53:57 +0800

    progress组件vuepress文档撰写

---
+++ docs/.vuepress/components/progress/progress.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="progress">
+    <div
+      class="progress-bar"
+      :class="{
+        'progress-bar-no-text': !showText,
+        'progress-bar-inside-text': inside
+      }"
+    >
+      <div class="progress-bar-outer" :style="{ height: `${height}px` }">
+        <div class="progress-bar-inner" :style="innerBarStyle">
+          <div class="progress-bar-inner-text" v-if="inside && showText">{{text}}</div>
+        </div>
+      </div>
+    </div>
+    <div class="progress-text" v-if="!inside && showText">{{text}}</div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'MoocProgress',
+  props: {
+    percentage: {
+      type: Number,
+      validator (val) {
+        return val >= 0 && val <= 100
+      }
+    },
+    color: {
+      type: String,
+      default: ''
+    },
+    height: {
+      type: [Number, String],
+      default: 6
+    },
+    showText: {
+      type: Boolean,
+      default: true
+    },
+    inside: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    innerBarStyle () {
+      return {
+        'width': `${this._percentage}%`,
+        'background-color': this.color
+      }
+    },
+    text () {
+      return `${this._percentage}%`
+    },
+    _percentage () {
+      return Math.max(0, Math.min(this.percentage, 100))
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  @import '../../stylus/variables.styl';
+  @import '../../stylus/src/progress-variables.styl';
+
+  .progress
+    position: relative;
+    line-height: 1;
+    & > div
+      display: inline-block;
+      vertical-align: middle;
+    .progress-bar
+      width: 100%;
+      margin-right: - $progress-bar-right;
+      padding-right: $progress-bar-right;
+      box-sizing: border-box;
+      &.progress-bar-no-text, &.progress-bar-inside-text
+        margin-right: 0;
+        padding-right: 0;
+      .progress-bar-outer
+        position: relative;
+        width: 100%;
+        height: $progress-bar-height;
+        border-radius: $progress-bar-radius;
+        background-color: $base-border-three-color;
+        overflow: hidden;
+        .progress-bar-inner
+          position: absolute;
+          left: 0;
+          top: 0;
+          height: 100%;
+          background-color: $base-primary;
+          white-space: nowrap;
+          text-align: right;
+          border-radius: $progress-bar-radius;
+          transition: width $progress-bar-duration ease;
+          .progress-bar-inner-text
+            display: inline-block;
+            vertical-align: middle;
+            margin: 0 5px;
+            color: #fff;
+            font-size: $progress-bar-inner-text-size;
+    .progress-text
+      margin-left: $progress-text-left;
+      line-height: 1;
+      font-size: $base-font-size;
+      color: $base-font-second-color;
+</style>
--- docs/components/base/progress.md
+++ docs/components/base/progress.md
@@ -2,6 +2,23 @@
 
 ## 用法
 
+### 基础用法
+<div class="progress-container">
+  <mooc-progress :percentage="67"></mooc-progress>
+</div>
+
+### 不同的颜色
+
+### 不同的高度
+
+### 是否显示进度文字
+
+### 进度文字是否内置
+
 ## 属性
 
-## 事件
+<style lang="stylus">
+  .progress-container
+    margin-top: 20px;
+    max-width: 600px;
+</style>
