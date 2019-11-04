401c173a181753cb8bfd4ea70f92b3571603cded
wangtunan <why583440138@gmail.com>
2019-11-04 20:28:18 +0800

    star组件实现vuepress文档撰写

--- src/pages/test/test.vue
+++ src/pages/test/test.vue
@@ -5,6 +5,13 @@
     <mooc-star v-model="value4" :max="10">
       <i slot="icon" class="iconfont">&#xe668;</i>
     </mooc-star>
+
+    <mooc-step :align-center="true">
+      <mooc-step-item title="未开始"></mooc-step-item>
+      <mooc-step-item title="开始"></mooc-step-item>
+      <mooc-step-item title="进行中"></mooc-step-item>
+      <mooc-step-item title="完结"></mooc-step-item>
+    </mooc-step>
   </div>
 </template>
 <script>
